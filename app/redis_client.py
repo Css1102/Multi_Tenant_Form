6f60564e6a5850c9ea22e816cec25464cdbf268f
Chiranjeev Singh Sethi <chiranjeev@Chiranjeevs-MacBook-Air.local>
2026-09-06 17:52:41 +0530

    fullstack_commit_5

--- app/redis_client.py
+++ app/redis_client.py
@@ -2,7 +2,7 @@ import redis
 import os
 import redis.asyncio as aioredis
 
-
+ 
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-sync_redis = redis.Redis.from_url(REDIS_URL, decode_responses=True)
-async_redis = aioredis.from_url(REDIS_URL, decode_responses=True)
+sync_redis = redis.Redis.from_url(REDIS_URL, decode_responses=True, ssl_cert_reqs=None)
+async_redis = aioredis.from_url(REDIS_URL, decode_responses=True, ssl_cert_reqs=None)
